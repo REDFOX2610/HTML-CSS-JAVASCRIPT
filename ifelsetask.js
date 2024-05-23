671a8cc769bcec3a8b93c05409afac9dabfee8bf
REDFOX2610 <pravincodedev@gmail.com>
2024-05-23 22:24:15 +0530

    switch case checking

--- ifelsetask.js
+++ ifelsetask.js
@@ -10,3 +10,17 @@ if (triangle[0] == triangle[1] && triangle[0] == triangle[2]) {
 } else {
   console.log("both side or not equal");
 }
+//-------------------------------------------\\
+var a = "this car is ";
+var car = "ford";
+switch (car) {
+  case "bmw":
+    console.log(a + "luxurios one");
+    break;
+  case "lamborgini":
+    console.log(a + "fastest one");
+    break;
+  case "ford":
+    console.log(a + "cheepest one");
+    break;
+}
